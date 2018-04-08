7e5df289963e9b079ea7bd82a9d0bcc5a3dc4c31
Matthew Shiel <mattshiel@gmail.com>
2018-04-08 15:54:21 +0100

    Started implementatipon of Shunting Yard algorithm

---
+++ shunt.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Converts an infix regexp to and returns a postfix regexp
+func Intopost(infix string) string {
+	pofix := []rune{}
+
+	return string(pofix)
+}
+
+func main() {
+
+	fmt.Println("Infix:     ", "a.b.c")
+	fmt.Println("Postfix:     ", Intopost("a.b.c"))
+
+}
