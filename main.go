bfbc72cd4bc7ccd4dc4354f8ca9d8a09ca64f4ee
Matthew Shiel <mattshiel@gmail.com>
2018-04-23 18:33:35 +0100

    Created basic skeleton for implementing thomson's construction

--- main.go
+++ main.go
@@ -1,15 +1,3 @@
-/*
- Author: Matthew Shiel
-
- References for the algorithm:
-	* https://en.wikipedia.org/wiki/Shunting-yard_algorithm,
-	* Video by lecturer Ian McLoughlin
-	* https://www.youtube.com/watch?v=Wz85Hiwi5MY
-
- References for understanding strings and runes:
-	* https://mymemorysucks.wordpress.com/2017/05/03/a-short-guide-to-mastering-strings-in-golang/
-*/
-
 package main
 
 import (
@@ -20,7 +8,7 @@ import (
 
 func main() {
 
-	/*var infix, postfix string
+	var infix, postfix string
 
 	//prompt the user to enter the infix string
 	fmt.Println("Enter infix expression to convert: ")
@@ -28,7 +16,5 @@ func main() {
 	//Convert string to postfix notation
 	postfix = shunt.IntToPost(infix)
 
-	fmt.Println("Converted expression is: " + postfix)*/
-	fmt.Println("Converted expression is: " + shunt.IntToPost("a.b.c*"))
-
+	fmt.Println("Converted expression is: " + postfix)
 }
---
+++ nfaEngine/nfaEngine.go
@@ -0,0 +1,34 @@
+/*
+ Author: Matthew Shiel
+
+ References for Thomson's construction in C and Golang:
+	* https://swtch.com/~rsc/regexp/regexp1.html,
+	* Video by lecturer Ian McLoughlin
+	* https://www.youtube.com/watch?v=Wz85Hiwi5MY
+
+ Refernces used for further understanding of the history behind the algorithm
+	* https://en.wikipedia.org/wiki/Thompson%27s_construction#Rules
+*/
+
+package nfaEngine
+
+type state struct {
+	symbol rune
+	edge1  *state
+	edge2  *state
+}
+
+type nfa struct {
+	initial *state
+	accept  *state
+}
+
+// poregtonfa converts a postfix regexp to an NFA and returns a pointer to it
+func poregtonfa(pofix string) *nfa {
+
+	// Stack structure
+	nfastack := []*nfa{}
+
+	return nfastack[0]
+
+}
