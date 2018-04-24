708ad89f99e51e802f48e2297484f0699e4897be
Matthew Shiel <mattshiel@gmail.com>
2018-04-25 00:50:24 +0100

    Improved main.go, tested NFA generation and string matching, all seems to work fine

--- main.go
+++ main.go
@@ -9,17 +9,54 @@ import (
 
 func main() {
 
-	var infix, postfix string
+	var infix string
+	var userInput string
 
-	//prompt the user to enter the infix string
-	fmt.Println("Enter infix expression to convert: ")
-	fmt.Scanln(&infix)
-	//Convert string to postfix notation
-	postfix = shunt.IntToPost(infix)
+	// Chose to use a simple scanner instead of a buffered reader
+	// If the program was expanded to where it needed to read in full sentences
+	// I would swap to a reader instead
 
-	fmt.Println("Converted expression is: " + postfix)
+	// Print menu options
+	fmt.Println("1 - Enter a regular expression in infix notation" + "\n2 - Exit")
+	fmt.Scanln(&userInput)
 
-	// Test to see if the pointer value to an NFA is returned
-	nfa := nfaEngine.Poregtonfa("ab.c*|")
-	fmt.Println(nfa)
+	for userInput != "2" {
+
+		// Main Menu options
+		switch userInput {
+		case "1":
+			// Prompt user to enter the infix regexp
+			fmt.Println("Enter infix expression to generate NFA: ")
+			fmt.Scanln(&infix)
+
+			//Convert infix regexp to postfix
+			postfix := shunt.IntToPost(infix)
+
+			NFAStringMatch(postfix)
+
+		default:
+			fmt.Println("Invalid choice please try again")
+		}
+		fmt.Scanln(&userInput)
+	}
+
+}
+
+// NFAStringMatch matches and tests a user string against
+// the user generated NFA
+func NFAStringMatch(postfix string) {
+
+	// User string to match against the NFA
+	var userString string
+
+	// Prompt user
+	fmt.Println("Enter a string to test against the NFA: ")
+	fmt.Scanln(&userString)
+
+	// Generate the NFA and test string against it
+	// then output the results
+	fmt.Print("Match Result: ")
+	fmt.Print(nfaEngine.PoMatch(postfix, userString))
+	fmt.Println("")
+
 }
